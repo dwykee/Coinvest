8c4cd762eac4d550e560e1c26e9efc4733c79645
dwykee <150531557+dwykee@users.noreply.github.com>
2026-06-24 22:50:23 +0700

    Update google migration

--- database/migrations/2026_06_14_192145_add_google_id_to_users_table.php
+++ database/migrations/2026_06_14_192145_add_google_id_to_users_table.php
@@ -12,8 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
+            if (!Schema::hasColumn('users', 'google_id')) {
             $table->string('google_id')->nullable()->after('email');
-        });
+    }
+});
     }
 
     public function down(): void
